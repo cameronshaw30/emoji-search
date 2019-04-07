c3400977ced8d077a0bd8d92420702b087d3b10c
cameronshaw <cameronwmshaw@gmail.com>
2019-04-07 14:35:17 +0100

    Renamed class to className for React

--- src/Contact.js
+++ src/Contact.js
@@ -5,46 +5,46 @@ class Contact extends PureComponent {
   render() {
     return (
         <section id="contact">
-          <div class="container">
-            <h2 class="text-center text-uppercase text-secondary mb-0">Contact Me</h2>
-            <hr class="star-dark mb-5"/>
-              <div class="row">
-                <div class="col-lg-8 mx-auto">
+          <div className="container">
+            <h2 className="text-center text-uppercase text-secondary mb-0">Contact Me</h2>
+            <hr className="star-dark mb-5"/>
+              <div className="row">
+                <div className="col-lg-8 mx-auto">
                   {/*To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19.*/}
                   {/*The form should work on most web servers, but if the form is not working you may need to configure your web server differently.*/}
                   <form name="sentMessage" id="contactForm" novalidate="novalidate">
-                    <div class="control-group">
-                      <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                    <div className="control-group">
+                      <div className="form-group floating-label-form-group controls mb-0 pb-2">
                         <label>Name</label>
-                        <input class="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name."/>
-                          <p class="help-block text-danger"></p>
+                        <input className="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name."/>
+                          <p className="help-block text-danger"></p>
                       </div>
                     </div>
-                    <div class="control-group">
-                      <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                    <div className="control-group">
+                      <div className="form-group floating-label-form-group controls mb-0 pb-2">
                         <label>Email Address</label>
-                        <input class="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address."/>
-                          <p class="help-block text-danger"></p>
+                        <input className="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address."/>
+                          <p className="help-block text-danger"></p>
                       </div>
                     </div>
-                    <div class="control-group">
-                      <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                    <div className="control-group">
+                      <div className="form-group floating-label-form-group controls mb-0 pb-2">
                         <label>Phone Number</label>
-                        <input class="form-control" id="phone" type="tel" placeholder="Phone Number" required="required" data-validation-required-message="Please enter your phone number."/>
-                          <p class="help-block text-danger"></p>
+                        <input className="form-control" id="phone" type="tel" placeholder="Phone Number" required="required" data-validation-required-message="Please enter your phone number."/>
+                          <p className="help-block text-danger"></p>
                       </div>
                     </div>
-                    <div class="control-group">
-                      <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                    <div className="control-group">
+                      <div className="form-group floating-label-form-group controls mb-0 pb-2">
                         <label>Message</label>
-                        <textarea class="form-control" id="message" rows="5" placeholder="Message" required="required" data-validation-required-message="Please enter a message."></textarea>
-                        <p class="help-block text-danger"></p>
+                        <textarea className="form-control" id="message" rows="5" placeholder="Message" required="required" data-validation-required-message="Please enter a message."></textarea>
+                        <p className="help-block text-danger"></p>
                       </div>
                     </div>
                     <br/>
                       <div id="success"></div>
-                      <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-xl" id="sendMessageButton">Send</button>
+                      <div className="form-group">
+                        <button type="submit" className="btn btn-primary btn-xl" id="sendMessageButton">Send</button>
                       </div>
                   </form>
                 </div>
--- src/Popups.js
+++ src/Popups.js
@@ -13,20 +13,20 @@ class Popups extends PureComponent {
         {/*Portfolio Modals*/}
 
         {/*Portfolio Modal 1*/}
-        <div class="portfolio-modal mfp-hide" id="portfolio-modal-1">
-            <div class="portfolio-modal-dialog bg-white">
-                <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                    <i class="fa fa-3x fa-times"></i>
+        <div className="portfolio-modal mfp-hide" id="portfolio-modal-1">
+            <div className="portfolio-modal-dialog bg-white">
+                <a className="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+                    <i className="fa fa-3x fa-times"></i>
                 </a>
-                <div class="container text-center">
-                    <div class="row">
-                        <div class="col-lg-8 mx-auto">
-                            <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                            <hr class="star-dark mb-5"/>
-                                <img class="img-fluid mb-5" src={cabin} alt=""/>
-                                    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                    <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                                        <i class="fa fa-close"></i>
+                <div className="container text-center">
+                    <div className="row">
+                        <div className="col-lg-8 mx-auto">
+                            <h2 className="text-secondary text-uppercase mb-0">Project Name</h2>
+                            <hr className="star-dark mb-5"/>
+                                <img className="img-fluid mb-5" src={cabin} alt=""/>
+                                    <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                    <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                                        <i className="fa fa-close"></i>
                                         Close Project</a>
                         </div>
                     </div>
@@ -35,20 +35,20 @@ class Popups extends PureComponent {
         </div>
 
         {/*Portfolio Modal 2*/}
-        <div class="portfolio-modal mfp-hide" id="portfolio-modal-2">
-          <div class="portfolio-modal-dialog bg-white">
-              <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                  <i class="fa fa-3x fa-times"></i>
+        <div className="portfolio-modal mfp-hide" id="portfolio-modal-2">
+          <div className="portfolio-modal-dialog bg-white">
+              <a className="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+                  <i className="fa fa-3x fa-times"></i>
               </a>
-              <div class="container text-center">
-                  <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                          <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                          <hr class="star-dark mb-5"/>
-                              <img class="img-fluid mb-5" src={cake} alt=""/>
-                                  <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                  <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                                      <i class="fa fa-close"></i>
+              <div className="container text-center">
+                  <div className="row">
+                      <div className="col-lg-8 mx-auto">
+                          <h2 className="text-secondary text-uppercase mb-0">Project Name</h2>
+                          <hr className="star-dark mb-5"/>
+                              <img className="img-fluid mb-5" src={cake} alt=""/>
+                                  <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                  <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                                      <i className="fa fa-close"></i>
                                       Close Project</a>
                       </div>
                   </div>
@@ -57,20 +57,20 @@ class Popups extends PureComponent {
         </div>
         
          {/*Portfolio Modal 3*/}
-        <div class="portfolio-modal mfp-hide" id="portfolio-modal-3">
-          <div class="portfolio-modal-dialog bg-white">
-              <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                  <i class="fa fa-3x fa-times"></i>
+        <div className="portfolio-modal mfp-hide" id="portfolio-modal-3">
+          <div className="portfolio-modal-dialog bg-white">
+              <a className="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+                  <i className="fa fa-3x fa-times"></i>
               </a>
-              <div class="container text-center">
-                  <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                          <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                          <hr class="star-dark mb-5"/>
-                              <img class="img-fluid mb-5" src={circus} alt=""/>
-                                  <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                  <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                                      <i class="fa fa-close"></i>
+              <div className="container text-center">
+                  <div className="row">
+                      <div className="col-lg-8 mx-auto">
+                          <h2 className="text-secondary text-uppercase mb-0">Project Name</h2>
+                          <hr className="star-dark mb-5"/>
+                              <img className="img-fluid mb-5" src={circus} alt=""/>
+                                  <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                  <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                                      <i className="fa fa-close"></i>
                                       Close Project</a>
                       </div>
                   </div>
@@ -79,20 +79,20 @@ class Popups extends PureComponent {
         </div>
         
          {/*Portfolio Modal 4*/}
-        <div class="portfolio-modal mfp-hide" id="portfolio-modal-4">
-          <div class="portfolio-modal-dialog bg-white">
-              <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                  <i class="fa fa-3x fa-times"></i>
+        <div className="portfolio-modal mfp-hide" id="portfolio-modal-4">
+          <div className="portfolio-modal-dialog bg-white">
+              <a className="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+                  <i className="fa fa-3x fa-times"></i>
               </a>
-              <div class="container text-center">
-                  <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                          <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                          <hr class="star-dark mb-5"/>
-                              <img class="img-fluid mb-5" src={game} alt=""/>
-                                  <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                  <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                                      <i class="fa fa-close"></i>
+              <div className="container text-center">
+                  <div className="row">
+                      <div className="col-lg-8 mx-auto">
+                          <h2 className="text-secondary text-uppercase mb-0">Project Name</h2>
+                          <hr className="star-dark mb-5"/>
+                              <img className="img-fluid mb-5" src={game} alt=""/>
+                                  <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                  <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                                      <i className="fa fa-close"></i>
                                       Close Project</a>
                       </div>
                   </div>
@@ -101,20 +101,20 @@ class Popups extends PureComponent {
         </div>
         
          {/*// Portfolio Modal 5*/}
-        <div class="portfolio-modal mfp-hide" id="portfolio-modal-5">
-          <div class="portfolio-modal-dialog bg-white">
-              <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                  <i class="fa fa-3x fa-times"></i>
+        <div className="portfolio-modal mfp-hide" id="portfolio-modal-5">
+          <div className="portfolio-modal-dialog bg-white">
+              <a className="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+                  <i className="fa fa-3x fa-times"></i>
               </a>
-              <div class="container text-center">
-                  <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                          <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                          <hr class="star-dark mb-5"/>
-                              <img class="img-fluid mb-5" src={safe} alt=""/>
-                                  <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                  <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                                      <i class="fa fa-close"></i>
+              <div className="container text-center">
+                  <div className="row">
+                      <div className="col-lg-8 mx-auto">
+                          <h2 className="text-secondary text-uppercase mb-0">Project Name</h2>
+                          <hr className="star-dark mb-5"/>
+                              <img className="img-fluid mb-5" src={safe} alt=""/>
+                                  <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                  <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                                      <i className="fa fa-close"></i>
                                       Close Project</a>
                       </div>
                   </div>
@@ -123,20 +123,20 @@ class Popups extends PureComponent {
         </div>
         
          {/*// Portfolio Modal 6*/}
-        <div class="portfolio-modal mfp-hide" id="portfolio-modal-6">
-          <div class="portfolio-modal-dialog bg-white">
-              <a class="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
-                  <i class="fa fa-3x fa-times"></i>
+        <div className="portfolio-modal mfp-hide" id="portfolio-modal-6">
+          <div className="portfolio-modal-dialog bg-white">
+              <a className="close-button d-none d-md-block portfolio-modal-dismiss" href="#">
+                  <i className="fa fa-3x fa-times"></i>
               </a>
-              <div class="container text-center">
-                  <div class="row">
-                      <div class="col-lg-8 mx-auto">
-                          <h2 class="text-secondary text-uppercase mb-0">Project Name</h2>
-                          <hr class="star-dark mb-5"/>
-                              <img class="img-fluid mb-5" src={submarine} alt=""/>
-                                  <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
-                                  <a class="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
-                                      <i class="fa fa-close"></i>
+              <div className="container text-center">
+                  <div className="row">
+                      <div className="col-lg-8 mx-auto">
+                          <h2 className="text-secondary text-uppercase mb-0">Project Name</h2>
+                          <hr className="star-dark mb-5"/>
+                              <img className="img-fluid mb-5" src={submarine} alt=""/>
+                                  <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                  <a className="btn btn-primary btn-lg rounded-pill portfolio-modal-dismiss" href="#">
+                                      <i className="fa fa-close"></i>
                                       Close Project</a>
                       </div>
                   </div>
