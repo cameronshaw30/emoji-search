b0fee1db012ace9b59ec56c7fd3161e7da51ecb5
cameronshaw <cameronwmshaw@gmail.com>
2019-04-07 16:41:25 +0100

    Start of Image component

--- src/Gallery.js
+++ src/Gallery.js
@@ -31,17 +31,6 @@ class Gallery extends PureComponent {
                     <div className="row">
                         <Image imageSource={images[0]} />
                         <div className="col-md-6 col-lg-4">
-                            <a className="portfolio-item d-block mx-auto" href="#portfolio-modal-1">
-                                <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
-                                    <div
-                                        className="portfolio-item-caption-content my-auto w-100 text-center text-white">
-                                        <i className="fas fa-search-plus fa-3x"></i>
-                                    </div>
-                                </div>
-                                <img className="img-fluid" src={images[0]} alt=""/>
-                            </a>
-                        </div>
-                        <div className="col-md-6 col-lg-4">
                             <a className="portfolio-item d-block mx-auto" href="#portfolio-modal-2">
                                 <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
                                     <div
--- src/Image.js
+++ src/Image.js
@@ -9,7 +9,16 @@ class Image extends PureComponent {
 
   render() {
     return (
-        <div><span>{this.imageSource}</span></div>
+        <div className="col-md-6 col-lg-4">
+          <a className="portfolio-item d-block mx-auto" href="#portfolio-modal-1">
+            <div className="portfolio-item-caption d-flex position-absolute h-100 w-100">
+              <div className="portfolio-item-caption-content my-auto w-100 text-center text-white">
+                <i className="fas fa-search-plus fa-3x"></i>
+              </div>
+            </div>
+            <img className="img-fluid" src={this.imageSource} alt=""/>
+          </a>
+        </div>
     );
   }
 }
